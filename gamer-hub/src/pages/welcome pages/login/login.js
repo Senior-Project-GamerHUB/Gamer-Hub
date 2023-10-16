29aacc57cb751a4e35404cf3f8d514e8969da90d
martmic <75264974+martmic@users.noreply.github.com>
2023-10-16 00:38:50 -0500

    Added some validation to the Login and SignUp page, as well as a few UI changes (you can remove if you like). The validation is not perfect but is a start, will be working on a connection to the database soon. (#6)

--- gamer-hub/src/pages/welcome pages/login/login.js
+++ gamer-hub/src/pages/welcome pages/login/login.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Valid from './loginValidation.js';
 
 const Login = () => {
+
+    const[values, setValues] = useState({
+        email: "",
+        password: ""
+    })
+   
+
+    const [errors, setErrors] = useState({})
+
+    const handleInput = (event) =>{
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+        console.log(event.target.name);
+        console.log(event.target.value);
+
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        setErrors(Valid(values));
+    }
+
     return(
         <div className="background-1">
             <section class="vh-100 gradient-custom">
@@ -11,27 +33,41 @@ const Login = () => {
                     <div class="card-body p-5 text-center">
 
                         <div class="mb-md-5 mt-md-4 pb-5">
-
                         <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
                         <p class="text-white-50 mb-5">Please enter your login and password!</p>
 
+
+                     <form action="" onSubmit= {handleSubmit}>
                         <div class="form-outline form-white mb-4">
-                            <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                            <label class="form-label" for="typeEmailX">Email</label>
+                        {errors.email && <span className = "text=danger" >{errors.email}</span>}
+                            <input 
+                            placeholder='Email'
+                            type="email" 
+                            id="typeEmailX" 
+                            class="form-control form-control-lg"
+                            onChange={handleInput}
+                            name = "email"
+                            />
                         </div>
 
                         <div class="form-outline form-white mb-4">
-                            <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                            <label class="form-label" for="typePasswordX">Password</label>
+                        {errors.password && <span className = "text=danger" >{errors.password}</span>}
+                            <input 
+                            placeholder='Password'
+                            type="password" 
+                            id="typePasswordX"
+                            class="form-control form-control-lg" 
+                            onChange={handleInput}
+                            name = "password"
+                            />
                         </div>
 
                         <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="/login">Forgot password?</a></p>
-
                         <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
 
+                    </form>
 
                         </div>
-
                         <div>
                         <p class="mb-0">Don't have an account? <a href="/signup" class="text-white-50 fw-bold">Sign Up</a>
                         </p>
--- gamer-hub/src/pages/welcome pages/signup/signup.js
+++ gamer-hub/src/pages/welcome pages/signup/signup.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './signup.css';
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+import Valid from './signupValidation';
 
 const SignUp = () => {
+
+    const[values, setValues] = useState({
+        name: "",
+        username:"",
+        email: "",
+        password: "",
+        repass:""
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const handleInput = (event) =>{
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+        console.log(event.target.name, event.target.value);
+
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        setErrors(Valid(values));
+    }
+   
+
+
     return(
+        <>
+
         <div className = "background-2">
         <section class="vh-100 gradient-custom">
         <div class="container py-5 h-100">
@@ -16,33 +46,90 @@ const SignUp = () => {
                     <h2 class="fw-bold mb-2 text-uppercase">Sign Up</h2>
                     <p class="text-white-50 mb-5">Create an Account to enter Gamer Hub</p>
 
+                <form action="" onSubmit={handleSubmit}>
+
                     <div class="form-outline form-white mb-4">
-                        <input type="name" id="typeNameX" class="form-control form-control-lg" />
-                        <label class="form-label" for="typeNameX">Name</label>
+                    {errors.name && <span className = "text=danger" >{errors.name}</span>}
+
+                        <input 
+                        name= "name"
+                        type="text" 
+                        id="typeNameX" 
+                        class="form-control form-control-lg" 
+                        placeholder='Name' 
+                        onChange={handleInput}
+    
+                        />
+                        
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <input type="username" id="typeUserNameX" class="form-control form-control-lg" />
-                        <label class="form-label" for="typeUsernameX">Username</label>
+                    {errors.username && <span className = "text=danger" >{errors.username}</span>}
+
+                        
+                        <img src= {user_icon} alt="" />
+                        <input 
+                        name= "username"
+                        type="text" 
+                        id="typeUserNameX" 
+                        class="form-control 
+                        form-control-lg" 
+                        placeholder='Username' 
+                        onChange={handleInput}
+                        />
+            
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                        <label class="form-label" for="typeEmailX">Email</label>
+                    {errors.email && <span className = "text=danger" >{errors.email}</span>}
+
+
+                        <img src= {email_icon} alt="" />
+                        <input 
+                        name= "email"
+                        type="email" 
+                        id="typeEmailX" 
+                        class="form-control form-control-lg" 
+                        placeholder='Email id'
+                        onChange={handleInput}
+                        />
+
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                        <label class="form-label" for="typePasswordX">Password</label>
+                    {errors.password && <span className = "text=danger" >{errors.password}</span>}
+
+
+                        <img src= {password_icon} alt="" />
+                        
+                        <input 
+                        name= "password"
+                        type="password" 
+                        id="typePasswordX"
+                        class="form-control form-control-lg" 
+                        placeholder='Password'
+                        onChange={handleInput} 
+                        />
                     </div>
 
                     <div class="form-outline form-white mb-4">
-                        <input type="repeatpassword" id="typeRepeatPasswordX" class="form-control form-control-lg" />
-                        <label class="form-label" for="typeRepeatPasswordX">Confirm Password</label>
+                    {errors.repass && <span className = "text=danger" >{errors.repass}</span>}
+
+
+                        <input 
+                        name= "repass"
+                        type="password" 
+                        id="typeRepeatPasswordX"
+                        class="form-control form-control-lg" 
+                        placeholder='Confirm Password' 
+                        onChange={handleInput}
+                        />
+
                     </div>
 
 
                     <button class="btn btn-outline-light btn-lg px-5" type="submit">Sign Up</button>
+                </form>
                     
                     </div>
 
@@ -57,6 +144,7 @@ const SignUp = () => {
         </div>
         </section>
         </div>
+        </>
     );
 };
 
